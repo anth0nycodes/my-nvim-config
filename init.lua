343b5228a41d07a9955d35e745c80424d3ab61d2
anth0nycodes <hoanganthony2207@gmail.com>
2025-10-20 12:26:03 -0400

    add

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd.colorscheme("tokyonight-night")
+vim.cmd.colorscheme("vscode_modern")
 
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("n", "<leader>q", ":bd<CR>", { noremap = true, silent = true })
