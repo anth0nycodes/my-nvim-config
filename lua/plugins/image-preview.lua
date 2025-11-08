ba65f9ea05f89324d1a304ef569f3da4100e6b79
anth0nycodes <hoanganthony2207@gmail.com>
2025-11-08 18:35:47 -0500

    remove

--- lua/plugins/image-preview.lua
+++
@@ -1,8 +0,0 @@
--- Note: doesn't work with ghostty terminal cause it doesn't support images yet
-return {
-  "skardyy/neo-img",
-  build = ":NeoImg Install",
-  config = function()
-    require("neo-img").setup()
-  end,
-}
