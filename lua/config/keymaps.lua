e8ccfa9aeb2b8ccdc96012f234da6f4283ee9368
anth0nycodes <hoanganthony2207@gmail.com>
2025-12-06 01:55:12 -0500

    remove unused keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<Tab>")', {
-  expr = true,
-  silent = true,
-  noremap = true,
-  replace_keycodes = false,
-})
-
 vim.keymap.set(
   "n",
   "<leader>sg",
