850e048c0667465306dc93570c98b91c5dff1943
anth0nycodes <hoanganthony2207@gmail.com>
2026-01-20 13:47:49 -0500

    thanks primeagen

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,6 +8,10 @@ vim.keymap.set(
   "<cmd>lua require('telescope.builtin').live_grep({additional_args = {'--hidden'}})<cr>"
 )
 
+-- Remap Ctrl-d and Ctrl-u to also center with zz
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down half a page and center cursor" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up half a page and center cursor" })
+
 -- Markdown Preview keymaps
 vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown Preview" })
 vim.keymap.set("n", "<leader>mc", "<cmd>MarkdownPreviewStop<cr>", { desc = "Markdown Preview Close" })
